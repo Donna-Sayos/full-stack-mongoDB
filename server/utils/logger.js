ecb5a32afddf70b28e1d3b17e59797be798686e3
Donna-Sayos <Donnasayos@gmail.com>
2023-01-20 13:33:59 -0500

    added color to logger

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,6 +1,12 @@
+const chalk = require("chalk");
+
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
-    next();
+  console.log(
+    chalk.greenBright(
+      `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    )
+  ); // for example: GET http://localhost:5000/api/users
+  next();
 };
 
 module.exports = logger;
