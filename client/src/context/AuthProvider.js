9995e50ed3ba6209f258a01ba08de1ddf68429ff
Donna-Sayos <Donnasayos@gmail.com>
2023-01-29 13:58:11 -0500

    added auth context

--- client/src/context/AuthProvider.js
+++ client/src/context/AuthProvider.js
@@ -1,2 +1,33 @@
 import React, { useReducer, createContext, useContext, useEffect } from "react";
-import
+import AuthReducer from "./AuthReducer";
+
+const INITIAL_STATE = {
+  user: JSON.parse(localStorage.getItem("user")) || null,
+  isFetching: false,
+  error: false,
+};
+
+const AuthContext = createContext(INITIAL_STATE);
+
+export default function AuthContextProvider({ children }) {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+  }, [state.user]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        value: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuthContext = () => useContext(AuthContext);
