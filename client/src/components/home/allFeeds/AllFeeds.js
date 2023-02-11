8455271e7d902ddd0c024a17ed2271649a4c8556
Donna-Sayos <Donnasayos@gmail.com>
2023-02-11 14:40:53 -0500

    allfeeds component finally up to date

--- client/src/components/home/allFeeds/AllFeeds.js
+++ client/src/components/home/allFeeds/AllFeeds.js
@@ -1,16 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../index.css";
 import Axios from "axios";
+import { useAuthContext } from "../../../context/AuthProvider";
 import Shares from "../../../common/share/Shares";
 import Post from "../../../common/post/Post";
 
 export default function AllFeeds() {
-  const [allPosts, setAllPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const desc = useRef();
+  const { user: currentUser } = useAuthContext();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    const newPost = {
+      userId: currentUser?._id,
+      desc: desc.current.value,
+    };
+
+    if (file) {
+      if (file.size > 10000000) {
+        // 10 MB
+        setError("File size is too large, max 10 MB allowed");
+        return;
+      }
+
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("uploadImg", file);
+      newPost.img = fileName;
+
+      try {
+        await Axios.post("/api/v1/upload", data);
+      } catch (err) {
+        console.error("Error uploading file: ", err.message);
+        setError("Error uploading file. Please try again later.");
+      }
+    }
+
+    try {
+      await Axios.post("/api/v1/posts", newPost);
+      const { data } = await Axios.get("/api/v1/posts/");
+      setPosts(
+        data.posts.sort(
+          (p1, p2) => new Date(p2.createdAt) - new Date(p1.createdAt)
+        )
+      );
+      desc.current.value = null;
+    } catch (err) {
+      console.error("Error creating post: ", err.message);
+      setError("Error creating post. Please try again later.");
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
       const { data } = await Axios.get("/api/v1/posts/");
-      setAllPosts(
+      setPosts(
         data.posts.sort(
           (p1, p2) => new Date(p2.createdAt) - new Date(p1.createdAt)
         )
@@ -23,9 +73,16 @@ export default function AllFeeds() {
   return (
     <div className="feed">
       <div className="feedWrapper">
-        <Shares />
-        {allPosts && allPosts.length > 0 ? (
-          allPosts.map((post) => <Post key={post._id} post={post} />)
+        <Shares
+          submitHandler={submitHandler}
+          file={file}
+          setFile={setFile}
+          desc={desc}
+          currentUser={currentUser}
+          error={error}
+        />
+        {posts && posts.length > 0 ? (
+          posts.map((post) => <Post key={post._id} post={post} />)
         ) : (
           <p className="empty">Be the first to make a post!</p>
         )}
