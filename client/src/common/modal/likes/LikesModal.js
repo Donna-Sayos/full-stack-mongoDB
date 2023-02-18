6f6b2f6c1df9103891783493e35514734563957c
Donna-Sayos <Donnasayos@gmail.com>
2023-02-17 19:23:47 -0500

    successfully moved likes modal on a different component

--- client/src/common/modal/likes/LikesModal.js
+++ client/src/common/modal/likes/LikesModal.js
@@ -1,103 +1,70 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./index.css";
-import { FaRegTimesCircle } from "react-icons/fa";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-export default function LikesModal({ post, allUsers }) {
-  const [likesArr, setLikesArr] = useState([]);
-
-  useEffect(() => {
-    let isSubscribed = true;
-    const fetchLikes = async () => {
-      const likes = await Promise.all(
-        post.likes.map((userId) => allUsers.find((u) => u._id === userId))
-      );
-      if (isSubscribed) setLikesArr(likes);
-    };
-
-    fetchLikes();
-
-    return () => (isSubscribed = false);
-  }, [allUsers, post.likes]);
-
-  if (!likesArr || likesArr.length === 0) {
+export default function LikesModal({ likers, show, handleClose }) {
+  if (likers.length === 0) {
     return (
-      <div
-        className="modal fade"
-        id="likesModal"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="likesModal"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-sm">
-          <div className="modal-content">
-            <div className="head modal-header">
-              <div className="d-flex align-items-center justify-content-start">
-                <img
-                  className="heart"
-                  src={"/assets/" + "others/heart.png"}
-                  alt="heart"
-                />
-                <span className="likesNum">0</span>
-              </div>
-              <div className="d-flex justify-content-end">
-                <button className="cancel" data-dismiss="modal">
-                  <FaRegTimesCircle size={22} />
-                </button>
-              </div>
-            </div>
-            <div className="modal-body">No likes yet.</div>
-          </div>
-        </div>
-      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <img
+              className="heart"
+              src={"/assets/" + "others/heart.png"}
+              alt="heart"
+            />
+            <span className="likesNum">0</span>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">No likes yet.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 
   return (
-    <div
-      className="modal fade"
-      id="likesModal"
-      tabIndex="-1"
-      role="dialog"
-      aria-labelledby="likesModal"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog modal-sm">
-        <div className="modal-content">
-          <div className="head modal-header">
-            <div className="d-flex align-items-center justify-content-start">
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          <img
+            className="heart"
+            src={"/assets/" + "others/heart.png"}
+            alt="heart"
+          />
+          <span className="likesNum">{likers.length} likes</span>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="likesContainer">
+        {likers &&
+          likers.map((user, index) => (
+            <div
+              key={index}
+              onClick={() => console.log(`${user.username} clicked...`)}
+            >
               <img
-                className="heart"
-                src={"/assets/" + "others/heart.png"}
-                alt="heart"
+                className="profileImg"
+                src={
+                  user.profilePicture
+                    ? "/assets/" + user.profilePicture
+                    : "/assets/" + "user/default-user-photo.png"
+                }
+                alt="user"
               />
-              {likesArr && likesArr.length > 0 ? (
-                <span className="likesNum">{likesArr.length}</span>
-              ) : null}
+              <span className="name">{user.username}</span>
+              {index !== likers.length - 1 && <hr className="likesHr" />}
             </div>
-            <div className="d-flex justify-content-end">
-              <button className="cancel" data-dismiss="modal">
-                <FaRegTimesCircle size={22} />
-              </button>
-            </div>
-          </div>
-          {likesArr &&
-            likesArr.map((u, index) => (
-              <div className="likers modal-body" key={index}>
-                <img
-                  className="profileImg"
-                  src={
-                    u?.profilePicture
-                      ? "/assets/" + u.profilePicture
-                      : "/assets/" + "user/default-user-photo.png"
-                  }
-                  alt="user"
-                />
-                <span className="name">{u?.username}</span>
-              </div>
-            ))}
-        </div>
-      </div>
-    </div>
+          ))}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
--- client/src/common/post/Post.js
+++ client/src/common/post/Post.js
@@ -5,9 +5,7 @@ import useFetchUsers from "../../utils/customHooks/UseFetchUsers";
 import Axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-// import LikesModal from "../modal/likes/LikesModal";
+import LikesModal from "../modal/likes/LikesModal";
 
 export default function Post({ post, posts, setPosts }) {
   const [like, setLike] = useState(post.likes.length);
@@ -158,62 +156,7 @@ export default function Post({ post, posts, setPosts }) {
                   <span>Be the first to like this</span>
                 )}
               </button>
-              {/* <LikesModal post={post} allUsers={allUsers} /> */}
-              {likers.length === 0 ? (
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>
-                      <img
-                        className="heart"
-                        src={"/assets/" + "others/heart.png"}
-                        alt="heart"
-                      />
-                      <span className="likesNum">0</span>
-                    </Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body className="text-center">No likes yet.</Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              ) : (
-                <Modal show={show} onHide={handleClose} key={user._id}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>
-                      <img
-                        className="heart"
-                        src={"/assets/" + "others/heart.png"}
-                        alt="heart"
-                      />
-                      <span className="likesNum">{likers.length} likes</span>
-                    </Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body className="likesContainer">
-                    {likers.map((user, index) => (
-                      <div key={index}>
-                        <img
-                          className="profileImg"
-                          src={
-                            user.profilePicture
-                              ? "/assets/" + user.profilePicture
-                              : "/assets/" + "user/default-user-photo.png"
-                          }
-                          alt="user"
-                        />
-                        <span className="name">{user.username}</span>
-                        {index !== likers.length - 1 && <hr className="likesHr" />}
-                      </div>
-                    ))}
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      Close
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              )}
+              <LikesModal likers={likers} show={show} handleClose={handleClose} />
             </div>
             <div className="postBottomRight">
               <button
