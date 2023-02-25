5e277bdaa70c5626eae149b6a2f515db9fa74ba3
Donna-Sayos <Donnasayos@gmail.com>
2023-02-25 13:46:07 -0500

    added a online chat component

---
+++ client/src/components/messenger/ChatOnline.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "./index.css";
+import Axios from "axios";
+import ProfilePic from "../../common/pic/ProfilePic";
+
+const chatOnlineImg = {
+  width: "40px",
+  height: "40px",
+  borderRadius: "50%",
+  objectFit: "cover",
+  border: "1px solid white",
+};
+
+export default function ChatOnline({
+  onlineUsers,
+  currentUserId,
+  setCurrentChat,
+}) {
+  const [friends, setFriends] = useState([]);
+  const [onlineFriends, setOnlineFriends] = useState([]);
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const res = await Axios.get("/api/v1/users/friends/" + currentUserId);
+      setFriends(res.data);
+    };
+
+    getFriends();
+  }, [currentUserId]);
+
+  useEffect(() => {
+    setOnlineFriends(friends.filter((f) => onlineUsers.includes(f._id)));
+  }, [friends, onlineUsers]);
+
+  const handleClick = async (user) => {
+    try {
+      const res = await Axios.get(
+        `/api/v1/conversations/find/${currentUserId}/${user._id}`
+      );
+      setCurrentChat(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="chatOnline">
+      {onlineFriends.map((online) => (
+        <div className="chatOnlineFriend" onClick={() => handleClick(online)}>
+          <div className="chatOnlineImgContainer">
+            <ProfilePic user={online} style={chatOnlineImg} />
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlineName">{online?.username}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
