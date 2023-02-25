0de018c5b308337238bc1484215aa963e1f6c33f
Donna-Sayos <Donnasayos@gmail.com>
2023-02-25 14:01:59 -0500

    added message component

---
+++ client/src/components/messenger/message/Message.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+import { format } from "timeago.js";
+
+export default function Message({ message, own }) {
+  return (
+    <div className={own ? "message own" : "message"}>
+      <div className="messageTop">
+        <img
+          className="messageImg"
+          src={"/assets/others/panda.png"}
+          alt="panda"
+        />
+        <p className="messageText">{message.text}</p>
+      </div>
+      <div className="messageBottom">{format(message.createdAt)}</div>
+    </div>
+  );
+}
