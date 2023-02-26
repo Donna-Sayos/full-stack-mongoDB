f5572b400808a44bb272a65e16d6015429502e85
Donna-Sayos <Donnasayos@gmail.com>
2023-02-26 13:07:06 -0500

    socket not giving error. but still not able to send message

--- client/src/components/messenger/Messenger.js
+++ client/src/components/messenger/Messenger.js
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:5001");
+    socket.current = io("http://localhost:5001");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
