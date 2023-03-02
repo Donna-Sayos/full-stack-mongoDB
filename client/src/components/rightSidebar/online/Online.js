0f7f12d4f6ed7a72094af5e438b44e1dcb77d59f
Donna-Sayos <Donnasayos@gmail.com>
2023-03-02 13:45:17 -0500

    used socket to check if user is online

--- client/src/components/rightSidebar/online/Online.js
+++ client/src/components/rightSidebar/online/Online.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../index.css";
 import { useAuthContext } from "../../../context/auth/AuthProvider";
+import io from "socket.io-client";
 import ProfilePic from "../../../common/pic/ProfilePic";
 import useFetchUsers from "../../../utils/customHooks/UseFetchUsers";
 
@@ -13,17 +14,39 @@ const rightSidebarProfileImg = {
 
 export default function Online({ userId }) {
   const { user: currentUser } = useAuthContext();
+  const [online, setOnline] = useState(false);
   const allUsers = useFetchUsers();
   const onlineUser = allUsers.find(
     (u) => u._id === userId && u._id !== currentUser._id
   );
+
+  useEffect(() => {
+    const socket = io("http://localhost:5001");
+
+    // When connected to the server, emit the "addUser" event to add the current user to the "users" array on the server-side.
+    socket.on("connect", () => {
+      socket.emit("addUser", currentUser._id);
+    });
+
+    // Listen to the "getUsers" event to update the online status of the user.
+    socket.on("getUsers", (users) => {
+      const online = users.some((user) => user.userId === onlineUser?._id);
+      setOnline(online);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [currentUser._id, onlineUser?._id]);
 
   return (
     <>
       <li className="rightSidebarFriend">
         <div className="rightSidebarProfileImgContainer">
           <ProfilePic style={rightSidebarProfileImg} user={onlineUser} />
-          <span className="rightSidebarOnline"></span>
+          <span
+            className={online ? "rightSidebarOnline" : "rightSidebarOffline"}
+          ></span>
         </div>
         <div className="rightSidebarUsername">
           {onlineUser?.username}{" "}
