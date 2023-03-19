bca5df305620bf45988758b0c7bd266cdbe17ec9
Donna-Sayos <Donnasayos@gmail.com>
2023-03-19 14:32:41 -0400

    created a chat context to handle open chats and notifications count

---
+++ client/src/context/chat/ChatContextProvider.js
@@ -0,0 +1,35 @@
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useMemo,
+} from "react";
+
+const ChatContext = createContext();
+
+export default function ChatContextProvider({ children }) {
+  const [selectedConversation, setSelectedConversation] = useState(null);
+  const [notification, setNotification] = useState([]);
+  const [chats, setChats] = useState(null);
+
+  const memoizedValues = useMemo(
+    () => ({
+      selectedConversation,
+      setSelectedConversation,
+      notification,
+      setNotification,
+      chats,
+      setChats,
+    }),
+    [selectedConversation, notification, chats]
+  );
+
+  return (
+    <ChatContext.Provider value={memoizedValues}>
+      {children}
+    </ChatContext.Provider>
+  );
+}
+
+export const useChatContext = () => useContext(ChatContext);
