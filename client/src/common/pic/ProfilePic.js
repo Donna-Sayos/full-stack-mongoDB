748ed85181e71725e3c092412e24c6f4050ed32e
Donna-Sayos <Donnasayos@gmail.com>
2023-04-19 15:14:54 -0400

    added userprofile pic to dependency array

--- client/src/common/pic/ProfilePic.js
+++ client/src/common/pic/ProfilePic.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
 
 export default function ProfilePic({ user, style }) {
+  const [currentUser, setCurrentUser] = useState(user);
+
+  useEffect(() => {
+    setCurrentUser(user);
+  }, [user, user.profilePicture]);
+
   return (
     <img
       className="prof"
       src={
-        user?.profilePicture
-          ? "/assets/" + user?.profilePicture
+        currentUser?.profilePicture
+          ? "/assets/" + currentUser?.profilePicture
           : "/assets/" + "user/default-user-photo.png"
       }
       alt="user"
@@ -15,4 +21,3 @@ export default function ProfilePic({ user, style }) {
     />
   );
 }
-
