e15815c3e09f9e950892b89ecfffbb40fe3096a9
Donna-Sayos <Donnasayos@gmail.com>
2023-04-19 15:58:12 -0400

    added route for read and unread controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,6 +9,8 @@ const {
   getSingleUser,
   deleteUser,
   updateUserPhoto,
+  markAsRead,
+  markAsUnread,
 } = require("../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
@@ -17,5 +19,7 @@ router.route("/:userId/userPicture").put(updateUserPhoto);
 router.route("/friends/:userId").get(getFriends);
 router.route("/:userId/follow").put(follow);
 router.route("/:userId/unfollow").put(unfollow);
+router.route("/:userId/markAsRead").put(markAsRead); // FIXME: testing feature
+router.route("/:userId/markAsUnread").put(markAsUnread); // FIXME: testing feature
 
 module.exports = router;
